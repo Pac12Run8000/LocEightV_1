10914a1d41bdf1da09933cd7928e506262a870ce
Pac12Run8000 <gangstarr78992@>
2020-04-24 21:53:58 -0700

    feat: working version

--- LocEightV_1/Controllers/HomeViewController.swift
+++ LocEightV_1/Controllers/HomeViewController.swift
@@ -604,37 +604,7 @@ extension HomeViewController {
         }
     
     
-    func shouldPresentLoadingView(status:Bool) {
-        var fadeView:UIView?
-        
-        if (status) {
-            fadeView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
-            fadeView?.backgroundColor = UIColor(red: 2/255, green: 64/255, blue: 89/255, alpha: 1.0)
-            fadeView?.alpha = 0.0
-            fadeView?.tag = 99
-            
-            let spinner = UIActivityIndicatorView()
-            spinner.color = UIColor.white
-            spinner.style = .large
-            spinner.center = view.center
-            
-            view.addSubview(fadeView!)
-            fadeView?.addSubview(spinner)
-            
-            spinner.startAnimating()
-            fadeView?.fadeTo(alphaValue: 0.7, withDuration: 0.2)
-        } else {
-            for subview in view.subviews {
-                if subview.tag == 99 {
-                    UIView.animate(withDuration: 0.2, animations: {
-                        subview.alpha = 0.0
-                    }) { (finished) in
-                        subview.removeFromSuperview()
-                    }
-                }
-            }
-        }
-    }
+    
 }
 
 
