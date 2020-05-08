7728c4f49394f5ceefc69c8463cc7efffbeec19a
Pac12Run8000 <gangstarr78992@>
2020-05-08 09:48:55 -0700

    Added images and now ready for the app store

---
+++ LocEightV_1/Controllers/LaunchViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LaunchViewController.swift
+//  LocEightV_1
+//
+//  Created by Michelle Grover on 5/8/20.
+//  Copyright © 2020 Norbert Grover. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        
+        view.backgroundColor = UIColor.blue1
+
+        
+    }
+    
+
+    
+
+}
